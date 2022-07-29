5d5270b701c554405a6a421cd0c9aa4b31dbddda
mkost148 <mkost148@gmail.com>
2022-07-30 01:47:29 +0300

    clean slighly CALC code

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -6,18 +6,20 @@ from brain_games.brain_servicelib import check_and_print_answer, hello
 def calc(num_of_attempts=3, difficulty=20):
     user_name = hello()
 
-    oper_sym = ('+', '-', '*')  # three operators are available
+    operators = ("+", "-", "*")  # three operators are available
     print('What is the result of the expression?')
     scores = 0
     while scores < num_of_attempts:
         num1 = random.randint(0, difficulty)
         num2 = random.randint(0, difficulty)
-        oper_type = random.randint(0, 2)  # three operators are available
-        print('Question:', str(num1), '', oper_sym[oper_type], '', str(num2))
 
-        if oper_type == 0:  # 'plus'
+        oper_sym = random.choice(operators)
+
+        print('Question:', str(num1), oper_sym, str(num2))
+
+        if oper_sym == "+":  # 'plus'
             correct_answer = num1 + num2
-        elif oper_type == 1:  # 'minus'
+        elif oper_sym == "-":  # 'minus'
             correct_answer = num1 - num2
         else:  # 'multiply'
             correct_answer = num1 * num2
